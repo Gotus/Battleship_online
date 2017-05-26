adfd45f907a6598282dd8475bf7de053a24b7d2d
selicon <stalker5430@mail.ru>
2017-05-26 21:17:03 +0300

    updated html. added test script for registration
    Signed-off-by: selicon <stalker5430@mail.ru>

--- src/main/resources/static/jsfiles/startpage.js
+++ src/main/resources/static/jsfiles/startpage.js
@@ -1,8 +1,13 @@
 /**
  * Created by stalk on 26.05.2017.
  */
-var login, password, checkonserver;
+var login, password, logincheck;
+var email, login2, password2, registrationcheck;
 
-checkonserver = function () {
+logincheck = function () {
     location.href = "localhost:8080/gamegate/login?login=" + login + "&password=" + password;
 };
+
+registrationcheck = function () {
+    location.href = "localhost:8080/gamegate/registrate?mail=" + email + "&login=" + login2 + "&password=" + password2;
+}
