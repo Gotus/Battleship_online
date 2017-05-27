9297c89865a9d1072fcd4bfb9fe74957577dc48c
selicon <stalker5430@mail.ru>
2017-05-27 17:12:42 +0300

    ajax test for startpage.html
    Signed-off-by: selicon <stalker5430@mail.ru>

--- src/main/resources/static/jsfiles/startpage.js
+++ src/main/resources/static/jsfiles/startpage.js
@@ -4,10 +4,43 @@
 var login, password, logincheck;
 var email, login2, password2, registrationcheck;
 
-logincheck = function () {
-    location.href = "/gamegate/login?login=" + login + "&password=" + password;
+/*logincheck = function () {
+    location.href = "http://localhost:8080/gamegate/login?login=" + login + "&password=" + password;
 };
 
 registrationcheck = function () {
-    location.href = "/gamegate/registrate?mail=" + email + "&login=" + login2 + "&password=" + password2;
-}
+    location.href = "http://localhost:8080/gamegate/registrate?mail=" + email + "&login=" + login2 + "&password=" + password2;
+};*/
+
+$(document).ready(function() {
+
+    $('#loginButton').click(function() {
+        if (login === "undefined") {
+            alert("login should not be empty");
+            return;
+        }
+        if (password === "undefined") {
+            alert("login should not be empty");
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/gamegate/login?login=" + login + "&password=" + password,
+            data: {
+                username: login,
+                password: password
+            },
+            success: function(data)
+            {
+                if (data !== null) {
+                    window.location.replace('admin/admin.php'); //lobies url
+                }
+                else {
+                    alert("incorrect login/password");
+                }
+            }
+        });
+    });
+
+});
