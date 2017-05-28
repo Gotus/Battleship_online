4fdc8fcd154bf3d2deef2acf269c1f25e65512c0
selicon <stalker5430@mail.ru>
2017-05-28 19:56:30 +0300

    returned startpage.js from test. names and id in buttons, buttons-groups
    Signed-off-by: selicon <stalker5430@mail.ru>

--- src/main/resources/static/jsfiles/startpage.js
+++ src/main/resources/static/jsfiles/startpage.js
@@ -29,11 +29,8 @@ $(document).ready(function () {
                 },
                 success: function (data) {
                     try {
-                        var format = '{"userID":1,"mail":"PPAP@mail.ru","login":"PPAP","password":"123654","dateOfRegistration":"May 11, 2017 12:00:00 AM","achievementsOfUser":[]}'
-                        object = JSON.parse(format);
-                        alert(object.mail);
-
-                        /*for (var key in object) {
+                        object = data;
+                        for (var key in object) {
                             //this if just checks if the key has a value, it is required
                             if (object.hasOwnProperty(key)) {
                                 var value = object[key];
@@ -45,7 +42,7 @@ $(document).ready(function () {
                                 sessionStorage.setItem(key, value);
                             }
                             location.href = "http://localhost:8080/profile.html";
-                        }*/
+                        }
                     } catch (e) {
                         // here you didn't get JSON back so we can assume it was an error, run your error code in here.  data will still be the error number (3)
                         return false;
