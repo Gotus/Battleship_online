321c3626b2597090376406ba78bff0d3655d5b55
selicon <stalker5430@mail.ru>
2017-05-29 15:58:23 +0300

    without try catch startpage.js
    Signed-off-by: selicon <stalker5430@mail.ru>

--- src/main/resources/static/jsfiles/startpage.js
+++ src/main/resources/static/jsfiles/startpage.js
@@ -6,34 +6,29 @@ var login, password, logincheck;
 $(document).ready(function () {
 
     $('#loginButton').click(function () {
-               login = $("#loginlogin").val();
-               password = $("#loginpassword").val();
-               if (login == "" || password == "") {
-                   alert("Please fill all fields...!!!!!!");
-               } else {
-               alert("pizdwes");
-                   $.ajax({
-                       url: "http://localhost:8080/gamegate/login",
-                       type: "POST",
-                       data: JSON.stringify({
-                           login: login,
-                           password: password
-                       }),
-                       contentType: 'application/json; charset=utf-8',
-                       dataType: "json",
-                       success: function (data) {
-                           try {
-                               if(data.isSuccess) {
-                                   location.href = "http://localhost:8080/profile.html";
-                               } else {
-                                   alert("Неверный логин/пароль");
-                               }
-                           } catch (e) {
-                               return false;
-                           }
-                       }
-                   })
-               }
-               return false;
+        login = $("#loginlogin").val();
+        password = $("#loginpassword").val();
+        if (login == "" || password == "") {
+            alert("Please fill all fields...!!!!!!");
+        } else {
+            $.ajax({
+                url: "http://localhost:8080/gamegate/login",
+                type: "POST",
+                data: JSON.stringify({
+                    login: login,
+                    password: password
+                }),
+                contentType: 'application/json; charset=utf-8',
+                dataType: "json",
+                success: function (data) {
+                    if (data.isSuccess) {
+                        location.href = "http://localhost:8080/profile.html";
+                    } else {
+                        alert("Неверный логин/пароль");
+                    }
+                }
+            })
+        }
+        return false;
     });
 });
