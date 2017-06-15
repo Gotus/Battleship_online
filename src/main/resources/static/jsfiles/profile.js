b4ebe7213b1696362dc3b865eba370309983302f
selicon <stalker5430@mail.ru>
2017-06-15 12:19:46 +0300

    trying to get achievements from server and mark got achievements
    Signed-off-by: selicon <stalker5430@mail.ru>

---
+++ src/main/resources/static/jsfiles/profile.js
@@ -0,0 +1,68 @@
+/**
+ * Created by stalk on 15.06.2017.
+ */
+var killstreak = $("#killstreakachievements");
+var missstreak = $("#missstreakachievements");
+var unique = $("#uniqueachievements");
+var saveddata = [];
+$(document).ready(function () {
+    $.ajax({
+        url: "/profile/getmyachievements",
+        type: "POST",
+        data: JSON.stringify({
+            login: mylogin
+        }),
+        contentType: 'application/json; charset=utf-8',
+        dataType: "json",
+        success: function (data) {
+            saveddata = data;
+            var column, achievement, img;
+            for (var i = 0; i < 1; i++) {
+                column = $('<div>').attr("class", "col-xs-4 col-sm-3 col-md-2 col-lg-2 no-padding");
+                achievement = $('<div>').attr("class", "achievement");
+                img = $('<img>').attr("src", "" + saveddata[i].path);
+                if (saveddata[i].gotAchievement){
+                    img.attr("class", "img img-responsive img-thumbnail got");
+                } else {
+                    img.attr("class", "img img-responsive img-thumbnail");
+                }
+
+                achievement.append(img);
+                column.append(achievement);
+                unique.append(column);
+            }
+
+            for (var i = 2; i <= 10; i++) {
+
+                column = $('<div>').attr("class", "col-xs-4 col-sm-3 col-md-2 col-lg-2 no-padding");
+                achievement = $('<div>').attr("class", "achievement");
+                img = $('<img>').attr("src", "" + saveddata[i].path);
+                if (saveddata[i].gotAchievement){
+                    img.attr("class", "img img-responsive img-thumbnail got");
+                } else {
+                    img.attr("class", "img img-responsive img-thumbnail");
+                }
+
+                achievement.append(img);
+                column.append(achievement);
+                killstreak.append(column);
+            }
+
+            for (var i = 11; i <= 16; i++) {
+
+                column = $('<div>').attr("class", "col-xs-4 col-sm-3 col-md-2 col-lg-2 no-padding");
+                achievement = $('<div>').attr("class", "achievement");
+                img = $('<img>').attr("src", "" + saveddata[i].path);
+                if (saveddata[i].gotAchievement){
+                    img.attr("class", "img img-responsive img-thumbnail got");
+                } else {
+                    img.attr("class", "img img-responsive img-thumbnail");
+                }
+
+                achievement.append(img);
+                column.append(achievement);
+                missstreak.append(column);
+            }
+        }
+    });
+});
